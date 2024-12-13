3a18e15122a586ee2fb20d5da3be51b458cc146f
ABHINAV ABHI <Abhinav Thonti>
2024-12-13 10:21:51 +0530

    first commit

---
+++ src/WeatherApp/WeatherApp.js
@@ -0,0 +1,176 @@
+import React, { useEffect, useState } from 'react'
+import './Weather.css'
+function WeatherApp() {
+    const [days, setDays] = useState([]);
+    const [data, setData] = useState(null);
+    const [input, setInput] = useState('')
+    const [city, setCity] = useState('Hyderabad');
+    const [daysIndex, setDaysIndex] = useState(0);
+    const [searchBtnFlag, setSearchBtnFlag] = useState(false);
+    const [inDate, setInDate] = useState(null);
+    async function getData() {
+        try {
+            let tempInput = input;
+            if (tempInput === '') {
+                tempInput = 'Hyderabad';
+            }
+              let tempDate=getDate();
+            let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${tempInput}/${tempDate}?unitGroup=us&include=days%2Cevents&key=7VNDDSKQZWW5GKCWREYN35BDC&contentType=json`)
+            if (response.status) {
+                if (response.status === 429) {
+                    alert("Too Many Requests!Try Again After Some Time!")
+                }
+            }
+            let temp = await response.json();
+            setCity(temp.resolvedAddress);
+            setDays(temp.days);
+            setData(temp.days[0]);
+            setSearchBtnFlag(false);
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+    function getDate() {
+        let dt;
+        if (inDate) {
+            dt = new Date(inDate);
+        }
+        else {
+            return '';
+        }
+        return (dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + "/" + dt.getFullYear() + "-" + (dt.getMonth() + 1) +"-28");
+    }
+    function searchClick() {
+        getData();
+    }
+    useEffect(() => {
+        getData();
+    }, [])
+
+    function clickNextDay() {
+        if (daysIndex !== days.length - 1) {
+            setData(days[daysIndex + 1]);
+            setDaysIndex((prev) => prev + 1)
+        }
+    }
+    function clickHome() {
+        setInDate(null);
+        getData();
+    }
+    function dateVisible() {
+        setSearchBtnFlag((prev) => !prev);
+    }
+    function setClick() {
+        getData();
+        setSearchBtnFlag(false);
+    }
+    return (
+
+        <div>
+            <div className='backGround h-lvh '>
+                <nav className='fixed w-[100%] bg-[rgb(255,255,255,0.3)] shadow-lg'>
+                    <div className='flex justify-around place-items-center'>
+                        <ul className='flex justify-center place-items-center gap-10 cursor-pointer text-xl text-white py-9'>
+                            <li onClick={clickHome} className='border-Animation px-2 border-b-[0.3rem] border-b-transparent anim'>Home</li>
+                            <li onClick={clickNextDay} className='border-b-[0.3rem] border-b-transparent anim'>Next Days Weather</li>
+                            <li onClick={dateVisible} className='border-b-[0.3rem] border-b-transparent anim'>Search by Date</li>
+                        </ul>
+                        {(searchBtnFlag) ? <div className='absolute top-20 left-[30%]'>
+                            <div className='text-xl overflow-hidden h-10 flex place-content-center rounded-lg shadow-xl'>
+                                <input type="date" className='p-2' onChange={(e) => setInDate(e.target.value)} value={inDate} />
+                                <button onClick={setClick} className='bg-blue-500 font-semibold text-white px-3 hover:bg-blue-400'>Set</button>
+                            </div>
+                        </div> : null}
+                        <div className='flex overflow-hidden rounded-lg text-xl'>
+                            <input type="text" placeholder='Search City...' className='w-72 px-4 py-2 focus:outline-none ' value={input} onChange={(e) => setInput(e.target.value)} />
+                            <button onClick={searchClick} className='bg-[rgb(0,206,209)] px-4 py-2 text-white font-semibold hover:bg-[rgb(96,229,232)] active:bg-[rgb(0,206,209)] duration-300'>Search</button>
+                        </div>
+                    </div>
+                </nav>
+                <div className='flex flex-col gap-16 p-7 pt-32'>
+                    {data ? <div className='text-2xl text-white flex flex-col gap-3 place-items-center'>
+                        <h1 className='border-b-2 w-fit px-4' >Weather For {city}</h1>
+                        <h1>On {data.datetime}</h1>
+                        <h1 className='border-b-2 w-fit px-4'>{data.description}</h1>
+                    </div> : null}
+                    <div className='flex justify-around text-2xl text-white px-16 flex-wrap'>
+                        <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 rounded-3xl flex flex-col gap-6 w-80 shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
+                                <p>Temperature:</p>
+                                {(data) ? <ul className='flex flex-col gap-4'><p className='text-5xl'>{data.temp} F</p>
+                                    <li>Temp-Max : {data.tempmax}</li>
+                                    <li>Temp-Min : {data.tempmin}</li>
+                                </ul>
+                                    : null}
+                            </div>
+                        </div>
+                        <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 rounded-3xl flex flex-col gap-6 w-80 shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
+                                <p>Humididty:</p>
+                                {data ? <ul className='flex flex-col gap-4'>
+                                    <p className='text-5xl'>{data.humidity} %</p>
+                                    <li>Feels Like : {data.feelslike}</li>
+                                    <li>Dew : {data.dew}</li>
+                                </ul> : null}
+                            </div>
+                        </div>
+                        <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 rounded-3xl flex flex-col gap-6 w-80 shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2", textShadow: "2px 4px 20px rgb(0,0,0,0.3)" }}>
+                                <p>Wind:</p>
+                                {data ? <ul className='flex flex-col gap-4'>
+                                    <p className='text-5xl'>{data.windspeed} Kmph</p>
+                                    <li>Wind-Gust : {data.windgust}</li>
+                                    <li>Direction : {data.winddir} deg</li>
+                                </ul> : null}
+                            </div>
+                        </div>
+                    </div>
+                    {(data && data.length !== 0) ?
+                        <div className='flex place-content-center w-[100%]'>
+                            <div className='flex text-xl px-10 text-[rgb(0,0,0,0.7)] gap-12 justify-around flex-wrap bg-[rgb(255,255,255,0.2)] rounded-xl shadow-xl p-10 '>
+                                <div>
+                                    <p>Pressure:</p>
+                                    <p>{data.pressure}</p>
+                                </div>
+                            
+                            <div>
+                                <p>Sunrise:</p>
+                                <p>{data.sunrise}</p>
+                            </div>
+                            <div>
+                                <p>Sunset:</p>
+                                <p>{data.sunset}</p>
+                            </div>
+                            <div>
+                                <p>Cloudcover:</p>
+                                <p>{data.cloudcover}</p>
+                            </div>
+                            <div>
+                                <p>UVindex</p>
+                                <p>{data.uvindex}</p>
+                            </div>
+                            <div>
+                                <p>SolarRadiation:</p>
+                                <p>{data.solarradiation}</p>
+                            </div>
+                            <div>
+                                <p>Precipitation:</p>
+                                <p>{data.precip}</p>
+                            </div>
+                            <div>
+                                <p>Moonphase:</p>
+                                <p>{data.moonphase}</p>
+                            </div>
+                            </div>
+                        </div>
+                        : null}
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default WeatherApp
