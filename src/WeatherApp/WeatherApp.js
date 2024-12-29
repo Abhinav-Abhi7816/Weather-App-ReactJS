0f0032187531ae07ff9ca6725044522f2b04c031
ABHINAV ABHI <Abhinav Thonti>
2024-12-29 21:45:28 +0530

    updated the website with new changes

--- src/WeatherApp/WeatherApp.js
+++ src/WeatherApp/WeatherApp.js
@@ -69,7 +69,7 @@ function WeatherApp() {
 
         <div>
             <div className='backGround w-[100%] bg-repeat  md:min-h-lvh '>
-                <nav className='fixed w-[100%] bg-[rgb(255,255,255,0.3)] shadow-lg'>
+                <nav className='fixed w-[100%] md:bg-[rgb(255,255,255,0.3)] shadow-lg bg-blue-300'>
                     <div className='flex justify-around place-items-center gap-0 md:flex-row flex-col'>
                         <ul className='flex justify-center place-items-center gap-10 cursor-pointer md:text-xl text-sm text-white md:py-9 py-2'>
                             <li onClick={clickHome} className='border-Animation px-2 border-b-[0.3rem] border-b-transparent anim'>Home</li>
@@ -96,17 +96,17 @@ function WeatherApp() {
                     </div> : null}
                     <div className='flex justify-around place-items-center md:text-3xl text-xl text-white md:px-16 md:gap-20 gap-3 flex-wrap'>
                         <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
-                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col md:gap-6 gap-2 md:w-96 min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col md:gap-6 gap-2 md:w-80  min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
                                 <p>Temperature:</p>
-                                {(data) ? <ul className='flex flex-col gap-4'><p className='md:text-6xl text-4xl'>{data.temp} F</p>
-                                    <li>Temp-Max : {data.tempmax}</li>
-                                    <li>Temp-Min : {data.tempmin}</li>
+                                {(data) ? <ul className='flex flex-col gap-4'><p className='md:text-6xl text-4xl'>{Math.floor(((data.temp) - 32) * 5/9 )}° C</p>
+                                    <li>Temp-Max : {Math.floor(((data.tempmax) - 32) * 5/9 )}° C</li>
+                                    <li>Temp-Min : {Math.floor(((data.tempmin) - 32) * 5/9 )}° C</li>
                                 </ul>
                                     : null}
                             </div>
                         </div>
                         <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
-                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col gap-6 md:w-96 min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col gap-6 md:w-80  min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
                                 <p>Humididty:</p>
                                 {data ? <ul className='flex flex-col gap-4'>
                                     <p className='md:text-6xl text-4xl'>{data.humidity} %</p>
@@ -116,12 +116,12 @@ function WeatherApp() {
                             </div>
                         </div>
                         <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
-                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col gap-6 md:w-96 min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2", textShadow: "2px 4px 20px rgb(0,0,0,0.3)" }}>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col gap-6 md:w-80  min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2", textShadow: "2px 4px 20px rgb(0,0,0,0.3)" }}>
                                 <p>Wind:</p>
                                 {data ? <ul className='flex flex-col gap-4'>
-                                    <p className='md:text-6xl text-4xl'>{data.windspeed} Kmph</p>
+                                    <p className='md:text-6xl text-4xl'>{data.windspeed} <span className='md:       text-5xl'>Kmph</span></p>
                                     <li>Wind-Gust : {data.windgust}</li>
-                                    <li>Direction : {data.winddir} deg</li>
+                                    <li>Direction : {data.winddir} <span className='md:text-xl'>deg</span></li>
                                 </ul> : null}
                             </div>
                         </div>
