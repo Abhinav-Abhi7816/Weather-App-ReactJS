965256523c070b0251638e97df2233d4fcb30b33
ABHINAV ABHI <Abhinav Thonti>
2024-12-29 21:22:27 +0530

    Updated the website with new changes

--- src/WeatherApp/WeatherApp.js
+++ src/WeatherApp/WeatherApp.js
@@ -68,37 +68,37 @@ function WeatherApp() {
     return (
 
         <div>
-            <div className='backGround h-lvh '>
+            <div className='backGround w-[100%] bg-repeat  md:min-h-lvh '>
                 <nav className='fixed w-[100%] bg-[rgb(255,255,255,0.3)] shadow-lg'>
-                    <div className='flex justify-around place-items-center'>
-                        <ul className='flex justify-center place-items-center gap-10 cursor-pointer text-xl text-white py-9'>
+                    <div className='flex justify-around place-items-center gap-0 md:flex-row flex-col'>
+                        <ul className='flex justify-center place-items-center gap-10 cursor-pointer md:text-xl text-sm text-white md:py-9 py-2'>
                             <li onClick={clickHome} className='border-Animation px-2 border-b-[0.3rem] border-b-transparent anim'>Home</li>
-                            <li onClick={clickNextDay} className='border-b-[0.3rem] border-b-transparent anim'>Next Days Weather</li>
+                            <li onClick={clickNextDay} className='border-b-[0.3rem] border-b-transparent anim'>See Next Day's</li>
                             <li onClick={dateVisible} className='border-b-[0.3rem] border-b-transparent anim'>Search by Date</li>
                         </ul>
-                        {(searchBtnFlag) ? <div className='absolute top-20 left-[30%]'>
-                            <div className='text-xl overflow-hidden h-10 flex place-content-center rounded-lg shadow-xl'>
+                        {(searchBtnFlag) ? <div className='absolute top-20 md:left-[28%]'>
+                            <div className='md:text-xl overflow-hidden h-10 flex place-content-center rounded-lg shadow-xl'>
                                 <input type="date" className='p-2' onChange={(e) => setInDate(e.target.value)} value={inDate} />
                                 <button onClick={setClick} className='bg-blue-500 font-semibold text-white px-3 hover:bg-blue-400'>Set</button>
                             </div>
                         </div> : null}
-                        <div className='flex overflow-hidden rounded-lg text-xl'>
-                            <input type="text" placeholder='Search City...' className='w-72 px-4 py-2 focus:outline-none ' value={input} onChange={(e) => setInput(e.target.value)} />
-                            <button onClick={searchClick} className='bg-[rgb(0,206,209)] px-4 py-2 text-white font-semibold hover:bg-[rgb(96,229,232)] active:bg-[rgb(0,206,209)] duration-300'>Search</button>
+                        <div className='flex overflow-hidden rounded-lg md:text-xl mb-3 md:mb-0'>
+                            <input type="text" placeholder='Search City...' className='md:w-72 px-4  focus:outline-none ' value={input} onChange={(e) => setInput(e.target.value)} />
+                            <button onClick={searchClick} className='bg-[rgb(0,206,209)] px-4 md:py-2 py-1 text-white font-semibold hover:bg-[rgb(96,229,232)] active:bg-[rgb(0,206,209)] duration-300'>Search</button>
                         </div>
                     </div>
                 </nav>
-                <div className='flex flex-col gap-16 p-7 pt-32'>
-                    {data ? <div className='text-2xl text-white flex flex-col gap-3 place-items-center'>
+                <div className='flex flex-col place-items-center md:gap-16 gap-6 p-7 md:pt-36 pt-24 w-[100%]'>
+                    {data ? <div className='md:text-3xl text-white flex flex-col md:gap-3 gap-1 place-items-center'>
                         <h1 className='border-b-2 w-fit px-4' >Weather For {city}</h1>
                         <h1>On {data.datetime}</h1>
                         <h1 className='border-b-2 w-fit px-4'>{data.description}</h1>
                     </div> : null}
-                    <div className='flex justify-around text-2xl text-white px-16 flex-wrap'>
+                    <div className='flex justify-around place-items-center md:text-3xl text-xl text-white md:px-16 md:gap-20 gap-3 flex-wrap'>
                         <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
-                            <div className='bg-[rgb(255,255,255,0.2)] p-7 rounded-3xl flex flex-col gap-6 w-80 shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col md:gap-6 gap-2 md:w-96 min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
                                 <p>Temperature:</p>
-                                {(data) ? <ul className='flex flex-col gap-4'><p className='text-5xl'>{data.temp} F</p>
+                                {(data) ? <ul className='flex flex-col gap-4'><p className='md:text-6xl text-4xl'>{data.temp} F</p>
                                     <li>Temp-Max : {data.tempmax}</li>
                                     <li>Temp-Min : {data.tempmin}</li>
                                 </ul>
@@ -106,20 +106,20 @@ function WeatherApp() {
                             </div>
                         </div>
                         <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
-                            <div className='bg-[rgb(255,255,255,0.2)] p-7 rounded-3xl flex flex-col gap-6 w-80 shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col gap-6 md:w-96 min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2" }}>
                                 <p>Humididty:</p>
                                 {data ? <ul className='flex flex-col gap-4'>
-                                    <p className='text-5xl'>{data.humidity} %</p>
+                                    <p className='md:text-6xl text-4xl'>{data.humidity} %</p>
                                     <li>Feels Like : {data.feelslike}</li>
                                     <li>Dew : {data.dew}</li>
                                 </ul> : null}
                             </div>
                         </div>
                         <div className='bg-[rgb(0,0,0,0.05)] rounded-3xl'>
-                            <div className='bg-[rgb(255,255,255,0.2)] p-7 rounded-3xl flex flex-col gap-6 w-80 shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2", textShadow: "2px 4px 20px rgb(0,0,0,0.3)" }}>
+                            <div className='bg-[rgb(255,255,255,0.2)] p-7 py-10 rounded-3xl flex flex-col gap-6 md:w-96 min-w-[300px] shadow-2xl' style={{ boxShadow: "2px 4px 20px rgb(0,0,0,0.2", textShadow: "2px 4px 20px rgb(0,0,0,0.3)" }}>
                                 <p>Wind:</p>
                                 {data ? <ul className='flex flex-col gap-4'>
-                                    <p className='text-5xl'>{data.windspeed} Kmph</p>
+                                    <p className='md:text-6xl text-4xl'>{data.windspeed} Kmph</p>
                                     <li>Wind-Gust : {data.windgust}</li>
                                     <li>Direction : {data.winddir} deg</li>
                                 </ul> : null}
@@ -128,7 +128,7 @@ function WeatherApp() {
                     </div>
                     {(data && data.length !== 0) ?
                         <div className='flex place-content-center w-[100%]'>
-                            <div className='flex text-xl px-10 text-[rgb(0,0,0,0.7)] gap-12 justify-around flex-wrap bg-[rgb(255,255,255,0.2)] rounded-xl shadow-xl p-10 '>
+                            <div className='flex md:text-2xl px-10 text-[rgb(0,0,0,0.7)] md:gap-12 gap-6 justify-around  flex-wrap bg-[rgb(255,255,255,0.2)] rounded-xl shadow-xl p-10 '>
                                 <div>
                                     <p>Pressure:</p>
                                     <p>{data.pressure}</p>
